2a1c4d7fb1eeabcdfcb464c001d172d79ccca357
Dorothée Catry <catrydorothee@gmail.com>
2025-11-25 15:31:37 +0100

    chore: setup pre-commit hooks for linting, formatting and security

--- app/database.py
+++ app/database.py
@@ -5,15 +5,18 @@ et fournit une fonction générateur pour obtenir des sessions de base de donné
 """
 
 import os
+from collections.abc import Generator
 
 from sqlmodel import Session, create_engine
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/items_db"
+)
 
 
 engine = create_engine(DATABASE_URL)
 
 
-def get_db():
+def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
